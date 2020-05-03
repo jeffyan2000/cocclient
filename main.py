dc1c1b0d3b5b29a1f0a3592eabbeaf02a0d8dced
Xuezhi Yan <2000chrisyan@gmail.com>
2020-05-02 22:27:27 -0300

    socketio handshake

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
 import socket
+import socketio
 
-UDP_IP = "localhost"
-UDP_PORT = 41234
-MESSAGE = "Hello, World!"
-
-sock = socket.socket(socket.AF_INET, # Internet
-             socket.SOCK_DGRAM) # UDP
-sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
-
+#udp receiving port
 UDP_PORT_RECEIVE = 5005
+#udp sending port
+UDP_PORT_SEND = 41234
+#tcp sending port
+TCP_PORT_SEND = 5006
+#destination IP
+UDP_IP = "localhost"
 
-sock_rec = socket.socket(socket.AF_INET, # Internet
-             socket.SOCK_DGRAM) # UDP
+#udp sender socket
+message = "Hello udp"
+sock_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock_send.sendto(bytes(message, "utf-8"), (UDP_IP, UDP_PORT_SEND))
 
-sock_rec.bind((UDP_IP, UDP_PORT_RECEIVE))
+#udp receiver socket
+sock_receive = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock_receive.bind((UDP_IP, UDP_PORT_RECEIVE))
+print("udp listening")
 
-print("listening")
+
+sio = socketio.Client()
+@sio.event
+def connect():
+    print('connection established')
+    sio.emit('info', "Hello tcp")
+
+@sio.event
+def my_message(data):
+    print('message received with ', data)
+    sio.emit('my response', {'response': 'my response'})
+
+@sio.event
+def disconnect():
+    print('disconnected from server')
+
+sio.connect('http://localhost:{}'.format(TCP_PORT_SEND))
+sio.wait()
+
 while True:
-    data, addr = sock_rec.recvfrom(1024) # buffer size is 1024 bytes
+    data, addr = sock_receive.recvfrom(1024) # buffer size is 1024 bytes
     print(data.decode("utf-8"))
